718576f9543830dd8175a04b6dad306a6280d06d
1to9only <53971101+1to9only@users.noreply.github.com>
2019-08-19 15:28:24 +0100

    Update pencilmarks.java

--- diuf/sudoku/test/pencilmarks.java
+++ diuf/sudoku/test/pencilmarks.java
@@ -121,6 +121,24 @@ public class pencilmarks {
                             cell.addPotentialValue(value);
                         }
                     }
+                    // fixup naked singles
+                    for (int i = 0; i < 81; i++) {
+                        Cell cell = grid.getCell(i % 9, i / 9);
+                        if ( cell.getPotentialValues().cardinality() ==  1 ) {
+                            int singleclue = cell.getPotentialValues().nextSetBit(0);
+                            boolean isnakedsingle = true;
+                            for (Cell housecell : cell.getHouseCells()) {
+                                if ( housecell.hasPotentialValue(singleclue) ) {
+                                    isnakedsingle = false;
+                                    break;
+                                }
+                            }
+                            if ( isnakedsingle ) {
+                                cell.setValue( singleclue);
+                                cell.getPotentialValues().clear();
+                            }
+                        }
+                    }               
                 }
                 else
                 {
