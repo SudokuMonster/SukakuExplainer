9ab4ab49c02799ee42528f255b37d6ffaa288f31
1to9only <53971101+1to9only@users.noreply.github.com>
2019-12-23 11:24:37 +0000

    re-fix Options -> Look & Feel fix

--- diuf/sudoku/gui/SudokuFrame.java
+++ diuf/sudoku/gui/SudokuFrame.java
@@ -361,9 +361,9 @@ public class SudokuFrame extends JFrame implements Asker {
 			Experimental =" (Explainer 1.2.1 mode)";
 		}
 		this.setTitle(ExSuffix + "Sukaku Explainer " + VERSION + "." + REVISION + SUBREV + Experimental);
+	  if (firstPass) {
         JMenuBar menuBar = getJJMenuBar();
-        if (firstPass)
-			setupLookAndFeelMenu();
+		setupLookAndFeelMenu();
         this.setJMenuBar(menuBar);
         this.setContentPane(getJContentPane());
         try {
@@ -371,6 +371,7 @@ public class SudokuFrame extends JFrame implements Asker {
         } catch (SecurityException ex) {
             // May happen in "applet" mode !
         }
+	  }
         this.getSudokuPanel().requestFocusInWindow();
     }
 
